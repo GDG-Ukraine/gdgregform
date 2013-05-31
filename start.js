69b3649750a24aeefdcd63a1d900333405864044
Valentyn Shybanov <olostan@gmail.com>
2013-05-31 16:17:33 +0300

    removy SPDY and Heroku config

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
-var app = require('./server/app').app,
-    spdy = require('./server/app.spdy').server;
-
+var app = require('./server/app').app
+//    ,spdy = require('./server/app.spdy').server
+;
 //spdy.listen(4243);
 app.listen(process.env.PORT || 4242);
 

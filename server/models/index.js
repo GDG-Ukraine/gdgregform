54ae8bad3438b2d5df6a2388d7c1a19fc3f237c6
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2015-11-16 15:58:21 +0200

    Replace deprecated hasMany method with belongsToMany

--- server/models/index.js
+++ server/models/index.js
@@ -2,8 +2,8 @@ require("fs").readdirSync("./server/models/").forEach(function(file) {
   if (file != 'index.js' && file[0] != '.') exports[file.substring(0,file.length-3)]=require("./" + file);
 });
 
-exports.participants.hasMany(exports.gevents, { as: 'Events', joinTableName: 'gdg_events_participation', 'foreignKey': 'googler_id'});
-exports.gevents.hasMany(exports.participants, { as: 'Participants', joinTableName: 'gdg_events_participation', 'foreignKey': 'event_id' });
+exports.participants.belongsToMany(exports.gevents, { as: 'Events', through: exports.participations, 'foreignKey': 'googler_id'});
+exports.gevents.belongsToMany(exports.participants, { as: 'Participants', through: exports.participations, 'foreignKey': 'event_id' });
 
 exports.gevents.belongsTo(exports.places,{'foreignKey': 'host_gdg_id', as:'Place'});
 
